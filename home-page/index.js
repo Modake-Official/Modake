155f3029eceaceba8ea40575c439218aa2ead204
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-27 17:05:05 +0700

    Thành công sử dụng gsap

--- home-page/index.js
+++ home-page/index.js
@@ -1,26 +1,12 @@
 console.log('START index.js');
-import { gsap } from "gsap";
-var productDownArrow; // Tạo biến
-var productButton;
+let productDownArrow = document.getElementsByClassName(
+    'fa-angle-down'
+)
+let productButton = document.getElementById('product')
 
 // Chạy flow
-addControls();
-addEvents();
 
-
-// Tạo hàm
-function addControls() {
-    productDownArrow = document.getElementsByClassName(
-        'fa-angle-down'
-    )
-    productButton = document.getElementById('product')
-}
-
-function addEvents() {
-    productButton.addEventListener('click', function () {
-        gsap.to(productDownArrow, {
-            rotation: 180,
-        })
-    });
-}
+gsap.to(['i', '.btn--blue'], {
+    rotation: 180,
+});
 
