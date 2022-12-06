31e5f54b2befadad9e0d1d7006bcd86874571bfa
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-06 22:48:51 +0700

    save

--- home-page/signup/signup.js
+++ home-page/signup/signup.js
@@ -1,15 +1,19 @@
 const inputEmail = document.getElementById('email');
 const inputUsername = document.getElementById('username');
 const inputPassword = document.getElementById('password');
-const buttonLogin = document.getElementById('btn-login');
+const buttonLogin = document.getElementById('btn-signup');
 
 checkButton()
 
 inputEmail.addEventListener('keyup', checkButton);
 inputPassword.addEventListener('keyup', checkButton);
 
+buttonLogin.addEventListener('click', function() {
+});
+
 function checkButton() {
-    if (inputEmail.value === '' || inputPassword.value === '' || inputUsername.value === '') {
+    if (inputEmail.value === '' || inputPassword.value === ''
+    || inputUsername.value === '' || checkEmail() === false) {
         console.log('Chưa nhập xong');
         buttonLogin.style.pointerEvents = 'none';
         buttonLogin.classList.add('btn--gray');
@@ -18,4 +22,11 @@ function checkButton() {
         buttonLogin.style.pointerEvents = 'auto';
         buttonLogin.classList.remove('btn--gray');
     }
+}
+
+// check Email with Regex
+function checkEmail() {
+    const email = inputEmail.value;
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return regex.test(email);
 }
